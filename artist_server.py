8c8e2548351913fa62cf3434e14f51a45850e55e
Vanhatai <Vanhatai@users.noreply.github.com>
2019-09-04 11:12:23 +0300

    Add files via upload

---
+++ artist_server.py
@@ -0,0 +1,69 @@
+# 1. Веб сервер принимает GET запросы по адресу /albums/<artist>  и выводит
+#   на экран сообщение с количеством альбомов исполнителя artist и списком
+#   названий этих альбомов.
+# 2. Веб сервер принимает POST запросы по адресу /albums/ и сохраняет переданные
+#   пользователем данные об альбоме. Данные передаются в формате веб формы.
+#   Если пользователь пытается передать данные об альбоме, который уже есть
+#   в базе данных, обработчик запроса отвечает HTTP ошибкой 409 и выводит
+#   соответствующее сообщение.
+
+# Структура базы данных album:
+# "id" integer primary key autoincrement,
+# "year" integer,
+# "artist" text,
+# "genre" text,
+# "album" text
+
+from bottle import route
+from bottle import run
+from bottle import HTTPError
+from bottle import request
+import re
+
+import bd
+
+# Выводим список альбомов по заданному artist. Пример: http://localhost:8080/albums/Queen
+@route("/albums/<artist>")
+def albums(artist):
+    albums_list = bd.find(artist)
+    if not albums_list:
+        message = f"Альбомов {artist} не найдено"
+        result = HTTPError(404, message)
+    else:
+        album_names = [album.album for album in albums_list]
+        result = f"Найдено {len(album_names)} альбомов {artist}:<br><br>"
+        result += "<br>".join(album_names)
+    return result
+
+@route("/albums", method="POST")
+def new_record():
+    # создаем словарь из POST-запроса
+    album_data = {
+        "artist": request.forms.get("artist"),
+        "genre": request.forms.get("genre"), 
+        "year": int(request.forms.get("year")),
+        "album": request.forms.get("album"),
+    }
+    print(album_data)
+    # блок валидации запросов пользователя
+    for record in album_data:
+        if not album_data[record]:
+            return f"Некорректные данные. Отсутствует поле '{record}'"
+    
+    # проверка rода
+    match = re.match(r"[1-2][0-9]{3}", str(album_data["year"]))
+    if not match:
+        return f"Некорректные данные. Неправильно указан год"
+    
+    new_album = bd.Album(**album_data)  # создаем экземпляр класса из словаря
+
+    # сохраняем новый альбом
+    if bd.save(new_album):
+        result = f"Альбом {album_data['album']} исполнителя {album_data['artist']} успешно сохранен"
+    else:
+        message = f"Альбом {album_data['album']} уже есть в базе"
+        result = HTTPError(409, message)
+    return result
+
+if __name__ == "__main__":
+    run(host="localhost", port=8080, server='tornado', debug=True)
